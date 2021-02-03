951a085d846dc8212b7414a269fcd9b480477321
sennur coban <sennurcoban97@hotmail.com>
2021-02-03 20:50:39 +0300

    Gelisme Süreci

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    //t-type
+    class MyList<T>
+    {
+        T[] items;//array bir referans olduğu için eğer alt tarafta(Add kısmı) çalıştırmak istersem
+        //onu constructor yardımıyla(ctor) newlemem gerek!
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            //dizinin eleman saysıını bir artırdım-bu şekilde elemanlarım kaybolur
+            //items = new T[items.Length+1];
+            T[] tempArray = items;//elemanları kaybetmemek için geçici array tutucu
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;//bu kısımda son arraya eklemek istediğimiz arrayi ekledik(T item)
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri gmusteri = new GercekMusteri();
+            gmusteri.Id = 1;
+            gmusteri.MusteriNo = "12345";
+            gmusteri.Adi = "Engin";
+            gmusteri.Soyadi = "Demiroğ";
+            gmusteri.TcNo = "2222222";
+
+            TuzelMusteri tmusteri = new TuzelMusteri();
+            tmusteri.Id = 2;
+            tmusteri.MusteriNo = "54321";
+            tmusteri.SirketAdi = "Kodlama.io";
+            tmusteri.VergiNo = "15462486";
+
+            Musteri gmusteri2 = new GercekMusteri();
+            Musteri tmusteri2 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(gmusteri);
+            musteriManager.Ekle(tmusteri);
+
+
+        }
+    }
+}
